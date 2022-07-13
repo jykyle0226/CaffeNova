2946bcd5df26bb7e9adc255f04bc520b6407432d
jykyle0226 <jykyle0226@gmail.com>
2022-07-13 10:11:32 -0400

    MVP complete

--- main_app/views.py
+++ main_app/views.py
@@ -9,7 +9,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -40,12 +39,13 @@ def signup(request):
     if form.is_valid():
       user = form.save()
       login(request, user)
-      return redirect('index')
+      return redirect('cafes')
     else:
       error_message = 'Invalid sign up - try again'
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+  
 
 
 class CafeCreate(LoginRequiredMixin, CreateView):
