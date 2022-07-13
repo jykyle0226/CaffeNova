b4e1bdd80a0b1853ba465bfd240dfd93622e5a28
jykyle0226 <jykyle0226@gmail.com>
2022-07-12 20:47:17 -0400

    more auth functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('about/', views.about, name='about'),
+  path('allCafes/', views.allCafe, name='allCafes'),
   path('cafes/', views.cafes_index, name='cafes'),
   path('cafes/<int:cafes_id>/', views.cafes_detail, name='detail'),
   path('cafes/create/', views.CafeCreate.as_view(), name='cafes_create'),
   path('cafes/<int:pk>/update/', views.CafeUpdate.as_view(), name='cafes_update'),
   path('cafes/<int:pk>/delete/', views.CafeDelete.as_view(), name='cafes_delete'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,34 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Cafe
 from django.shortcuts import render
 from .models import Cafe
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 def home(request):
   return render(request, 'home.html')
 
-def about(request):
-  return render(request, 'about.html')
-
-def cafes_index(request):
+@login_required
+def allCafe(request):
   cafes = Cafe.objects.all()
+  return render(request, 'allCafe.html', {'cafes': cafes})
+
+@login_required
+def cafes_index(request):
+  cafes = Cafe.objects.filter(user=request.user)
   return render(request, 'cafes/index.html', {'cafes': cafes})
 
+@login_required
 def cafes_detail(request, cafes_id):
   cafes = Cafe.objects.get(id=cafes_id)
   return render(request, 'cafes/detail.html', {'cafes': cafes})
 
-class CafeCreate(CreateView):
+
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
+
+class CafeCreate(LoginRequiredMixin, CreateView):
+  model = Cafe
+  fields = '__all__'
+  success_url = '/cafes/'
+  
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class CafeUpdate(LoginRequiredMixin, UpdateView):
   model = Cafe
   fields = '__all__'
   success_url = '/cafes/'
 
-class CafeUpdate(UpdateView):
-  model = Cafe
-  fields = '__all__'
-  success_url = '/cafes/'
-
-class CafeDelete(DeleteView):
+class CafeDelete(LoginRequiredMixin, DeleteView):
   model = Cafe
   success_url = '/cafes/'
