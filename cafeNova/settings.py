490ad22e4243b5c08b2c93330ceba9b90907574c
jykyle0226 <jykyle0226@gmail.com>
2022-09-15 12:56:14 -0400

    css change

--- cafeNova/settings.py
+++ cafeNova/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = '/cafes/'
 LOGOUT_REDIRECT_URL = '/'
+
+GOOGLE_API_KEY = 'AIzaSyCr789VAsSZqmVZ9SOjpH7t78sUVQwZScs'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
